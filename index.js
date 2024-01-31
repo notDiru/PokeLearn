cbf4cca3758be1c8a77b0cf85a9f85716f5b7d0a
Diru <angeldiazdiru93@gmail.com>
2024-01-31 04:27:23 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import express, { response } from "express"
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+const API_URL = "https://pokeapi.co/api/v2"
+let err = ""
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+});
+
+app.get("/search-pokemon", (req, res) => {
+    res.redirect("/");
+})
+
+app.post("/search-pokemon", (req, res) => {
+
+    fetchData(req.body.name.toLowerCase(), (info, error) => {
+        if(error){
+            return res.render("index.ejs", {
+                error: "Has escrito bien el pokemon?"
+            })
+        }
+        res.render("index.ejs", {
+            image: info.sprites?.front_default,
+            name: info.name,
+            type: info.types[0]?.type.name,
+            type2: info.types[1]?.type.name,
+            error: ""
+        }) 
+    })
+});
+
+function fetchData(endpoint, callback) {
+    fetch(`${API_URL}/pokemon/${endpoint}`)
+        .then(response => response.json())
+        .then(data => callback(data))
+        .catch(error => {
+            callback(null, "error");
+        });
+
+}
+
+app.listen(port,() => {
+    console.log("Server runnin' on port: " + port);
+});
